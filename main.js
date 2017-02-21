5dd4c9a70e32d8b13d2d0ed95cba302359e03d87
an nguyen <hello.an.ng@gmail.com>
2017-02-21 10:10:03 -0800

    solves challenge 2

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
 console.log('hello from main.js');
 
+// var btnText = btn.innerHTML;
 var btn = document.querySelector('button');
-var btnText = btn.innerHTML;
+var counter = 0;
+const input = document.querySelector('input');
+input.focus();
 
 function handleBtn (evt) {
   // console.log('button clicked');
   // console.log(btnText);
-  btn.innerHTML = btnText++;
+  // btn.innerHTML = btnText++;
+  btn.textContent = counter += 1;
 }
 btn.addEventListener('click', handleBtn);
 
+const colors = [];
+const newColorForm = document.querySelector('#new-color-form');
 
+function handleInput (evt) {
+  evt.preventDefault();
+  const color = document.querySelector('#new-color').value;
+  colors.push(color);
+  // change background
+  changeBackground(color);
+  // render list
+  renderColors(colors);
+}
+// input.addEventListener('keypress', handleInput);
+newColorForm.addEventListener('submit', handleInput);
+
+function changeBackground(color) {
+  document.body.style.backgroundColor = color;
+}
+
+// const
+function renderColors(colors) {
+  const html = colors.map( color => `<li>${color}</li>`).join('');
+  document.querySelector('#colors').innerHTML = html;
+}
+newColorForm.addEventListener('submit', handleInput);
