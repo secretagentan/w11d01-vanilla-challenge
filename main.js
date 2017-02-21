09288f5ae87bf222f0c09c53c270797e3685fbcf
an nguyen <hello.an.ng@gmail.com>
2017-02-21 09:46:55 -0800

    solves challenge 1

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-main.js
+console.log('hello from main.js');
+
+var btn = document.querySelector('button');
+var btnText = btn.innerHTML;
+
+function handleBtn (evt) {
+  // console.log('button clicked');
+  // console.log(btnText);
+  btn.innerHTML = btnText++;
+}
+btn.addEventListener('click', handleBtn);
+
+
